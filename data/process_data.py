18619a2838bd53be6c28ca8e2a59a70627f6d1a7
rob <robert.siwicki@googlemail.com>
2021-01-18 18:28:45 +0000

    clean and load added

--- data/process_data.py
+++ data/process_data.py
@@ -41,11 +41,13 @@ def load_data(messages_filepath, categories_filepath):
     return df
     
 def clean_data(df):
-    
-
+    # drop duplicates
+    df= df.drop_duplicates(subset=["message"],keep="first")
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('messages', engine, index=False)
 
 
 def main():
